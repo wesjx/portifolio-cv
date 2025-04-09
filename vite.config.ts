817e8a219a877097c5c531fbc1078340b5989437
Wesley Junio <wesjrpiri@gmail.com>
2025-04-09 17:39:07 +0100

    Updated to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/portifolio-cv/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
 })
 
 // https://vitejs.dev/config/
